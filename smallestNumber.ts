d492e5987fdf91f7517f54d02ea80f8089b70f01
mathiskc04mca27-coder <mathiskc04_mca27@mepcoeng.ac.in>
2026-03-31 10:31:45 +0530

    Create smallestNumber.ts

---
+++ smallestNumber.ts
@@ -0,0 +1,19 @@
+function nextGreatestLetter(letters: string[], target: string): string {
+    let low = 0;
+    let high = letters.length - 1;
+
+    while (low <= high) {
+        const mid = Math.floor(low + (high - low) / 2);
+
+        if (letters[mid] <= target) {
+            
+            low = mid + 1;
+        } else {
+      
+            high = mid - 1;
+        }
+    }
+
+    
+    return letters[low % letters.length];
+}
